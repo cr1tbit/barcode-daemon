f0adfffbd0ff7e8ba57b26560c0ab1cc7112ace2
critbit <js@vcc.earth>
2019-10-06 15:08:48 +0100

    object-wrapping and threading the app

--- barcode_daemon/__main__.py
+++ barcode_daemon/__main__.py
@@ -1,50 +1,82 @@
 #!/usr/bin/python
 import struct
-import time
+from time import sleep
 import sys
 import socket
-
-char_table = {
-    2: "1",
-    3: "2",
-    4: "3",
-    5: "4",
-    6: "5",
-    7: "6",
-    8: "7",
-    9: "8",
-    10: "9",
-    11: "0",
-    28: "\n"
-    }
-
-infile_path = "/dev/input/event" + (sys.argv[1] if len(sys.argv) > 1 else "0")
-
-FORMAT = 'llHHI'
-EVENT_SIZE = struct.calcsize(FORMAT)
-
-#open event handler file in binary mode
-in_file = open(infile_path, "rb")
-
-#read the scanner in loop
-event = in_file.read(EVENT_SIZE)
-
-sock = socket.socket(socket.AF_INET, 
-                     socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-while event:
-    (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
-   
-    #these events represent virtual keypress coming from the scanner
-    if type == 1 and value == 1:
-        c = char_table.get(code,"")
-        print(c,end = '')
-        sock.sendto(c.encode('utf8'),('<broadcast>',2137))
-
-    event = in_file.read(EVENT_SIZE)
-
-in_file.close()
+import queue
+from threading import Thread
+import logging
 
 
 
+class InputEventWrapper:
+    def __init__(self, filepath:str = None):
+        if filepath is None:
+            filepath = "/dev/input/event0" 
+       
+        self.input_fh = open(filepath, "rb")
+        logging.info("started event wrapper on file "+str(filepath))
+    
+        self.event_format = 'llHHI'
+        self.event_size = \
+            struct.calcsize(self.event_format)
+    
+        self.char_table = {
+            2: "1", 3: "2", 4: "3",
+            5: "4", 6: "5", 7: "6",
+            8: "7", 9: "8", 10: "9",
+            11: "0", 28: "\n"
+        }
+        
+    def unpack_event(self, event:bytes) -> dict:
+        (tv_sec, tv_usec, type, code, value) = struct.unpack(self.event_format, event)
+        return {
+            'type':type,
+            'code':code,
+            'value':value
+            }
+
+    #careful - blocking functions ahead
+    def get_event(self) -> dict:
+        event = self.input_fh.read(self.event_size)
+        return self.unpack_event(event)
+        
+    def get_letter(self) -> str:
+        event_dict = self.get_event()
+        if event_dict['type'] == 1 and event_dict['value'] == 1:
+            return self.char_table.get(event_dict['code'],"")
+
+    def get_barcode(self) -> str:
+        barcode = ""
+        while True:
+            c = self.get_letter()
+            if c is '\n':
+                return barcode
+            if c is not None:
+                barcode +=c
+
+
+barcode_queue = queue.Queue()
+
+def input_reader() -> None:
+    wrap = InputEventWrapper()
+    while True:
+        barcode_queue.put(wrap.get_barcode())
+
+def barcode_emitter() -> None:
+    while True:
+        while not barcode_queue.empty():
+            print("barcode: " + str(barcode_queue.get()))
+        sleep(0.1)
+
+t_emmiter = Thread(target=barcode_emitter)
+t_emmiter.daemon = True
+t_emmiter.start()
+
+t_event_reader = Thread(target=input_reader)
+t_event_reader.daemon=True
+t_event_reader.start()
+
+logging.info("entring eternal slumber.")
+while True:
+    sleep(10000)
